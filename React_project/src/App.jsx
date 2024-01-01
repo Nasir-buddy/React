d9f292794ade034e3c57277c92ac45b547005743
nasir-buddy <syedali172000@gmail.com>
2024-01-01 12:29:01 +0530

    Passing value from/in components

--- React_project/src/App.jsx
+++ React_project/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Products from './Products'
 
 function App() {
+      var [a,b] = useState(14);
   return (
     <>
       <div className="w-screen h-screen bg-zinc-400 p-4">
-        <div className='w-44 h-32 rounded-xl bg-red-600'>
-          <h1 className='p-3'>My Name is Nasir Ali</h1>
+        <div className='w-80 h-80 rounded-xl bg-red-300'>
+          <Products name="passingValue" data={{age:23 , name: "Nasir Ali"}}/>
         </div>
-      <Products/>
       </div>
     </>
   )
