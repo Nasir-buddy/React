1a79b5d1319487f2d6b9f39b04b73012587765c6
nasir-buddy <syedali172000@gmail.com>
2024-02-18 00:12:31 +0530

    React: Conditional Rendering in React

--- React_project/src/Components/Card.jsx
+++ React_project/src/Components/Card.jsx
@@ -1,33 +1,38 @@
 import React from "react"
 function Card() {
     const data = [{
-        image: 'https://images.unsplash.com/photo-1563203369-26f2e4a5ccf7?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        name: "Amazon Basic",
-        description: "Hey my name is nasir ali and i study in accurate institute of management and technology"
-    },
-    {
-        image: 'https://images.unsplash.com/photo-1519865885898-a54a6f2c7eea?q=80&w=916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        name: "Amazon",
-        description: " accurate institute of management and technology"
-    },
-    {
-        image: 'https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        image: 'https://images.unsplash.com/photo-1563203369-26f2e4a5ccf7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: "Apple",
-        description: "Hey my name is nasir ali "
+        description: 'Hey my name is nasir ali and i study in accurate institute of management and technology.',
+        inStock: true
+    },{
+        image: 'https://plus.unsplash.com/premium_photo-1666264200762-f2ae0643b9ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        name: "Amazon",
+        description: 'Accurate institute of management and technology.',
+    inStock: false        
+    },{
+        image: 'https://images.unsplash.com/photo-1590005354167-6da97870c757?q=80&w=2081&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        name: "Basic",
+        description: 'Hey my name is nasir Ali',
+        inStock: false
     }];
+    const clickHandeler = ()=>{
+        alert("Clicked the button by the user.")
+    }
     return (
-        <div className="w-full h-screen bg-zinc-200 flex items-center justify-center gap-10">
-            {data.map((value, index) => (
-                <div key={index} className=" w-52 bg-zinc-100 overflow-hidden rounded-md">
-                    <div className="w-full h-32 bg-zinc-300">
-                        <img className="w-full h-full object-fit-cover" src={value.image}></img>
-                    </div>
-                    <div className="w-full px-3 py-4">
-                        <h2 className="font-semibold">{value.name}</h2>
-                        <p className="text-xs mt-5">{value.description}</p>
-                    </div>
+        <div className="bg-zinc-500 w-full h-screen flex justify-center items-center gap-10">
+           {data.map((value, index)=>(
+            <div key={index} className="w-52  bg-zinc-300 rounded-md overflow-hidden">
+                <div className="w-full h-32">
+                    <img className="w-full h-full object-fit-cover" src={value.image}></img>
                 </div>
-            ))}
+                <div className="w-full h-full px-4 py-3">
+                    <h2 className="font-semibold ">{value.name}</h2>
+                    <p className="text-xs mt-5">{value.description}</p>
+            <button onClick={clickHandeler} className={`p-2 bg-blue-500 mt-4 rounded-md ${value.inStock ? 'bg-green-500': 'bg-red-500'}`}>{value.inStock ? 'Show more':'Not Available'}</button>
+                </div>
+            </div>
+           ))}
         </div>
     )
 }
