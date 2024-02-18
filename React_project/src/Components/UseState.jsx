e1acef5a585bb5b02321c2a7dc8f34cba49f5ab3
nasir-buddy <syedali172000@gmail.com>
2024-02-19 01:15:11 +0530

    React: UseState change in array

--- React_project/src/Components/UseState.jsx
+++ React_project/src/Components/UseState.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react'
 
 function UseState() {
-    const [val, setval] = useState([1,2,3,4,5]);
-  return (
-    <>
-    {val.map(item =><h1>{item}</h1>)}
-    <button className='
-    p-2 
-    bg-blue-400 
-    rounded-xl'
-    onClick={
-        ()=>{setval(()=>{ return val.filter((item) => item % 2 !== 0)})}
-    }
-    >click</button>
-    
-    </>
-  )
+    const [val, setval] = useState([
+        { name: "Nasir Ali", age: 23 },
+        { name: "Prachi Karn", age: 22 },
+        { name: "Adarsh Raj", age: 21 }
+    ]);
+    return (
+        <>
+            <div>
+                {val.map((item) => (
+                    <div>
+                        <h1>{item.name}</h1>
+                        <h2>{item.age}</h2>
+                    </div>
+                ))}
+                <button className='bg-blue-300 p-2 rounded-xl mt-2'
+                    onClick={() => setval(
+                        val.map(
+                            (item => item.name === "Nasir Ali" ? ({ name: "Nasir", age: 20 }) : ({ name: "Prachi", age: 20 })
+                            )))}
+                >Click</button>
+            </div>
+        </>
+    )
 }
 
 export default UseState 
