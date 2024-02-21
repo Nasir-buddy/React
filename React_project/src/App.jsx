f0e2af09ed68f27994ce7361126e8d2a4d0b5956
nasir-buddy <syedali172000@gmail.com>
2024-02-21 15:54:10 +0530

    React:  Created a Friend and Unfriend Button

--- React_project/src/App.jsx
+++ React_project/src/App.jsx
@@ -6,11 +6,24 @@ import Props from './Components/Props'
 import DummyCard from './Components/DummyCard'
 function App() {
   const raw = [
-    {name: "Nasir Ali", Profession: "Coder", image: 'https://images.unsplash.com/photo-1480997173806-f69bb3b35b84?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {name: "Abhay Yadav", Profession: "Artist", image: 'https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {name: "Adarsh Raj", Profession: "Business Man", image: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {name: "Archil Sachan", Profession: "Momos Wala", image: 'https://www.tripsavvy.com/thmb/gYRhS5rFVb3Xz3y7xvzY9FrExS4=/2120x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-503908259-59493c463df78c537bf936d8.jpg'}
+    {name: "Nasir Ali", Profession: "Coder", image: 'https://images.unsplash.com/photo-1480997173806-f69bb3b35b84?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', friend:false},
+    {name: "Abhay Yadav", Profession: "Artist", image: 'https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', friend:false},
+    {name: "Adarsh Raj", Profession: "Business Man", image: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', friend:false},
+    {name: "Archil Sachan", Profession: "Momos Wala", image: 'https://www.tripsavvy.com/thmb/gYRhS5rFVb3Xz3y7xvzY9FrExS4=/2120x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-503908259-59493c463df78c537bf936d8.jpg', friend:false}
   ]
+
+  const [realData, setData] = useState(raw);
+  const handlClick = (cardIndex)=>{
+      setData((prev)=>{
+        return prev.map((item, index)=>{
+          if(index === cardIndex){
+            return {...item, friend: !item.friend}
+          }
+          return item;
+        })
+      })
+  }
+ 
   return (
     <>
       {/* <Card/> */}
@@ -19,8 +32,8 @@ function App() {
       {/* <Props text='Know More' color='bg-blue-600'/>
        <Props text='Download' color='bg-red-700'/> */}
        <div className='bg-zinc-300 h-screen w-full flex items-center justify-center gap-4'>
-     {raw.map((item, index)=>(
-      <DummyCard value={item}/>
+     {realData.map((item, index)=>(
+      <DummyCard key={index} index={index} friendButtonHandle={handlClick} value={item}/>
      ))}
        </div>
     </>
--- React_project/src/Components/DummyCard.jsx
+++ React_project/src/Components/DummyCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function DummyCard({value}) {
-    const {image, name, Profession} = value;
+function DummyCard({value, friendButtonHandle, index}) {
+    const {image, name, Profession, friend} = value;
     return (
         <div className='w-60 h-65 overflow-hidden rounded-xl bg-zinc-100'>
             <div className='w-full h-41 rounded-md overflow-hidden'>
@@ -10,7 +10,7 @@ function DummyCard({value}) {
             <div className='w-full p-4 h-full '>
                 <h2 className='text-black font-semibold text-xl'>{name}</h2>
                 <h2 className='text-black font-semibold text-sm'>{Profession}</h2>
-                <button className='bg-blue-300 p-2 rounded-md mt-2'>Add Friend</button>
+                <button onClick={()=>{friendButtonHandle(index)}}  className={`${friend === true ? "bg-red-500": "bg-blue-300"} bg-blue-300 p-2 rounded-md mt-2`}>{friend === true ? "Friend": "Add Friend"}</button>
             </div>
         </div>
     )
