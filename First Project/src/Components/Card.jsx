73a63ee7b20604e5d894cf236eb1432427bed3cf
nasir-buddy <syedali172000@gmail.com>
2024-02-25 01:57:16 +0530

    Project : Completed Card part

--- First Project/src/Components/Card.jsx
+++ First Project/src/Components/Card.jsx
@@ -9,7 +9,7 @@ function Card({data}) {
       <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/80 text-white overflow-hidden px-8 py-10'>
         <FaFileCode />
         <p className='font-semibold text-sm leading-tight mt-5'>{data.desc}</p>
-        <div className='footer absolute bottom-0 w-full   left-0'>
+        <div className='footer absolute bottom-0 w-full left-0'>
           <div className='flex items-center justify-between px-8 py-3 mb-3'>
           <h5>{data.fileSize}</h5>
           <span className='w-7 h-7 rounded-full  bg-zinc-600 flex items-center justify-center'>
@@ -17,8 +17,8 @@ function Card({data}) {
           </span>
           </div>
           {
-            data.tag.isOpen && (<div className='tag w-full py-4 h-10 bg-green-600 flex items-center justify-center'>
-            <h3 className='text-sm font-semibold'>Download Now</h3>
+            data.tag.isOpen && (<div className={`tag w-full py-4 h-10 ${data.tag.tagColor === "blue" ? "bg-blue-500" : "bg-green-500"} flex items-center justify-center`}>
+            <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
           </div>)
           }
         </div>
--- First Project/src/Components/Foreground.jsx
+++ First Project/src/Components/Foreground.jsx
@@ -6,12 +6,30 @@ function Foreground() {
         {
              desc: "This is the background color of the card that will be displayed", 
              fileSize: ".9mb", 
-             close : true, 
-             tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green"},
+             close : false, 
+             tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+        },
+        {
+            desc: "This is the background color of the card that will be displayed", 
+            fileSize: ".9mb", 
+            close : false, 
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue"},
+       },
+       {
+        desc: "This is the background color of the card that will be displayed", 
+        fileSize: ".9mb", 
+        close : true, 
+        tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green"},
+   },
+   {
+             desc: "This is the background color of the card that will be displayed", 
+             fileSize: ".9mb", 
+             close : false, 
+             tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue"},
         },
     ]
     return (
-        <div className='fixed w-full h-screen top-0 left-0 z-[3]'>
+        <div className='fixed w-full h-screen top-0 left-0 z-[3] p-5 flex flex-wrap gap-5'>
            {data.map((item, index)=>(
             <Card key={index} data={item}/>
            ))}
