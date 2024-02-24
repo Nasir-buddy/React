edeafbc9889f1de42d476b4499ab6d143f6c6240
nasir-buddy <syedali172000@gmail.com>
2024-02-25 01:16:08 +0530

    Project : Created full Card

--- First Project/src/Components/Foreground.jsx
+++ First Project/src/Components/Foreground.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from './Card'
 
 function Foreground() {
     return (
-        <div>
-            <div className='fixed w-full h-screen top-0 left-0 z-[3]'></div>
+        <div className='fixed w-full h-screen top-0 left-0 z-[3]'>
+            <Card />
         </div>
+
     )
 }
 
