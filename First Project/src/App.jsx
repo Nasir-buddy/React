5b8159b26cd1c8aee95fec7343ec2ef8b64709b7
nasir-buddy <syedali172000@gmail.com>
2024-02-25 00:16:10 +0530

    Project: Created Foreground

--- First Project/src/App.jsx
+++ First Project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Background from './Components/Background'
+import Foreground from './Components/Foreground'
 function App() {
   return (
     <div className='relative h-screen w-full bg-zinc-800 '>
       <Background />
-      
+      <Foreground/>
     </div>
   )
 }
---
+++ First Project/src/Components/Foreground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Foreground() {
+    return (
+        <div>
+            <div className='fixed w-full h-screen top-0 left-0 z-[3]'></div>
+        </div>
+    )
+}
+
+export default Foreground
