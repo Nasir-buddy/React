60594e78ade3d9a0f282574d3cbeb61f436c4e42
nasir-buddy <syedali172000@gmail.com>
2024-02-25 01:38:36 +0530

    Project : Dymanic Card Data

--- First Project/src/Components/Foreground.jsx
+++ First Project/src/Components/Foreground.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+    const data = [
+        {
+             desc: "This is the background color of the card that will be displayed", 
+             fileSize: ".9mb", 
+             close : true, 
+             tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green"},
+        },
+    ]
     return (
         <div className='fixed w-full h-screen top-0 left-0 z-[3]'>
-            <Card />
+           {data.map((item, index)=>(
+            <Card key={index} data={item}/>
+           ))}
         </div>
 
     )
