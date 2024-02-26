acd10d0d290765e6cc8b47fe415a0884df137325
nasir-buddy <syedali172000@gmail.com>
2024-02-26 06:20:55 +0530

    React: All method to showing style

--- Routing/src/Components/Nav.jsx
+++ Routing/src/Components/Nav.jsx
@@ -4,24 +4,29 @@ function Nav() {
     return (
         <>
             <nav className='w-full h-20 flex justify-center items-center gap-4 '>
-                <NavLink className={(e)=>{
+            {/* By using the css style properites  */}
+                <NavLink style={(e) => {
+                    return {
+                        color: e.isActive ? 'red' : '',
+                        fontWeight: e.isActive ? 'bold' : '',
+                    }
+                }} to={"/"}>Home</NavLink>
+                {/* By using the className in tailwindcss properties  */}
+                <NavLink className={(e) => {
                     return [
-                        e.isActive ? 'text-red-300' : '', 
+                        e.isActive ? 'text-red-300' : '',
                         e.isActive ? 'font-bold' : ''
                     ].join(' ');
-                }} to={"/"}>Home</NavLink>
-                <NavLink className={(e)=>{
-                    return [
-                        e.isActive? 'text-red-300' : '', 
-                        e.isActive? 'font-bold' : ''
-                    ].join(' ');
                 }} to={"/about"}>About</NavLink>
-                <NavLink className={(e)=>{
-                    return [
-                        e.isActive? 'text-red-300' : '', 
-                        e.isActive? 'font-bold' : ''
-                    ].join(' ');
-                }} to={"/user"}>User</NavLink>
+                {/* By using the little bit change direct returning the tag  */}
+                <NavLink to={"/user"}>{(e) => {
+                    return (
+                        <span className={[
+                            e.isActive ? 'text-red-300' : '',
+                            e.isActive ? 'font-bold' : ''
+                        ].join(' ')} >User</span>
+                    )
+                }}</NavLink>
             </nav>
         </>
     )
