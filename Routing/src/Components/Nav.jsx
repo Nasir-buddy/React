a2b0d5e3d7a0172ecba6dc73f2afcdb062603ab2
nasir-buddy <syedali172000@gmail.com>
2024-02-26 05:45:16 +0530

    React: implement style in css in NavLink

--- Routing/src/Components/Nav.jsx
+++ Routing/src/Components/Nav.jsx
@@ -4,9 +4,24 @@ function Nav() {
     return (
         <>
             <nav className='w-full h-20 flex justify-center items-center gap-4 '>
-                <NavLink to={"/Home"}>Home</NavLink>
-                <NavLink to={"/about"}>About</NavLink>
-                <NavLink to={"/user"}>User</NavLink>
+                <NavLink style={(e) => {
+                    return {
+                        color: e.isActive ? "red" : "",
+                        fontWeight: e.isActive ? 'bold' : ''
+                    }
+                }} to={"/Home"}>Home</NavLink>
+                <NavLink style={(e) => {
+                    return {
+                        color: e.isActive ? 'red' : '',
+                        fontWeight: e.isActive ? 'bold' : ''
+                    }
+                }} to={"/about"}>About</NavLink>
+                <NavLink style={(e) => {
+                    return {
+                        color: e.isActive ? 'red' : '',
+                        fontWeight: e.isActive ? 'bold' : ''
+                    }
+                }} to={"/user"}>User</NavLink>
             </nav>
         </>
     )
