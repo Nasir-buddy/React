2f2b358d2c330e40bcaf37425ceaa18eeb67e4a0
nasir-buddy <syedali172000@gmail.com>
2024-02-27 18:17:09 +0530

    React : Dynamic Routing

---
+++ Routing/src/Components/UserDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserDetail() {
+  return (
+    <div className='w-1/2 m-auto'>
+        <h1 className='text-xl font-semibold text-[2vw] mb-10 text-red-500'>User User Details</h1>
+        <h1 className='text-xl mb-5'>User Name</h1>
+        <button className='bg-red-200 rounded-md px-3 py-2'>Go Back</button>
+    </div>
+  )
+}
+
+export default UserDetail
--- Routing/src/utils/Routing.jsx
+++ Routing/src/utils/Routing.jsx
@@ -3,12 +3,14 @@ import Home from '../Components/Home'
 import About from '../Components/About'
 import User from '../Components/User'
 import { Route, Routes } from 'react-router-dom'
+import UserDetail from '../Components/UserDetail'
 function Routing() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
-            <Route path='/user' element={<User />} />
+            <Route path='/user/' element={<User />} />
+            <Route path='/user/:name' element={<UserDetail />} />
         </Routes>
     )
 }
