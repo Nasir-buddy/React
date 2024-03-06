30c72ac004af36391725333e7410f0c07fbde073
nasir-buddy <syedali172000@gmail.com>
2024-03-07 02:06:16 +0530

    React Context: import data in component from exported data from Context

--- Context API/src/components/User.jsx
+++ Context API/src/components/User.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../context/Context';
 function User() {
+  const { user, setUser } = useContext(UserContext);
+
   return (
-    <div>User</div>
+    <div>
+      <h1 className='text-xl m-2'>User List</h1>
+      <div className='w-1/4 bg-blue-200 rounded-md p-1 m-2'>
+        {user.map((u)=>(
+          <Link to={'/user/${u.id}'} className='flex m-2 bg-red-200 rounded-md p-2'>
+          {u.username}
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
