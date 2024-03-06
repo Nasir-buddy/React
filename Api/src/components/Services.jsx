910a2d2951dd92dab18dc0d02ac747f710076d3f
nasir-buddy <syedali172000@gmail.com>
2024-03-06 20:02:30 +0530

    React useEffect: updating data in components

--- Api/src/components/Services.jsx
+++ Api/src/components/Services.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Services() {
+    const [first, setFirst] = useState("This is normal data");
+    const [second, setSecond] = useState("This is very large data");
+    useEffect(()=>{
+        console.log("Service component is created");
+        return ()=>{
+            console.log("Service component is destroyed");
+        }
+    })
   return (
-    <div>Services</div>
+    <div>
+        <h1>{first}</h1>
+        <button onClick={()=>setFirst("normal data has been changed")} className='bg-red-200 rounded-md px-3 py-1 mb-2 mt-2'>Change Normal Data</button>
+
+        <h1>{second}</h1>
+        <button onClick={()=>setSecond("large data has been changed")} className='bg-red-200 rounded-md px-3 py-1 mb-2 mt-2'>Change large Data</button>
+    </div>
   )
 }
 
