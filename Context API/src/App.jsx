d4ac21766176f383828e0b646f47ab5de9905540
nasir-buddy <syedali172000@gmail.com>
2024-03-07 01:00:53 +0530

    React context API: created routes

--- Context API/src/App.jsx
+++ Context API/src/App.jsx
@@ -1,35 +1,26 @@
 import React, { useState } from 'react'
+import Home from './components/Home'
+import { Link, Route, Routes } from 'react-router-dom'
+import User from './components/User'
 
 function App() {
-  const [user, setUser] = useState({
-    "id": 1,
-    "email": "john@gmail.com",
-    "username": "johnd",
-    "password": "m38rmF$",
-    "name": {
-      "firstname": "john",
-      "lastname": "doe"
-    },
-    "phone": "1-570-236-7033",
-    "__v": 0
-  },
-    {
-      "id": 2,
-      "email": "morrison@gmail.com",
-      "username": "mor_2314",
-      "password": "83r5^_",
-      "name": {
-        "firstname": "david",
-        "lastname": "morrison"
-      },
-      "phone": "1-570-236-7033",
-      "__v": 0
-    },
-  );
+  
 
   return (
     <div className='w-full h-screen bg-zinc-200'>
-      App
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/user'>User</Link>
+      <Link to='/about'>About</Link>
+    </nav>
+
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/user' element={<User/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+    </Routes>
+
+      <Home/>
     </div>
   )
 }
