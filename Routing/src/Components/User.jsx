779ce9f51c939ef701207710da301b54f9ad82ec
nasir-buddy <syedali172000@gmail.com>
2024-03-06 14:36:43 +0530

    React Routing: React Outlet

--- Routing/src/Components/User.jsx
+++ Routing/src/Components/User.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 function User() {
   return (
     <div className='w-1/2 m-auto'>
-    <h1 className='text-xl font-semibold text-[2vw] mb-10'>User</h1>
-    <p className='text-md font-semibold '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni dolores exercitationem consequaturatur nostrum voluptates? Temporibus quibusdam reiciendis ea laboriosam, labore minima illo aliquam!</p>
-</div>
+      <h1 className='text-xl font-semibold text-[2vw] mb-10'>User</h1>
+      <div className='flex flex-col mt-5 w-1/2'>
+        <Link className='bg-red-200 p-3 hover:bg-red-300 mb-3 rounded-md' to="/user/nasir-ali">Nasir Ali</Link>
+        <Link className='bg-red-200 p-3 hover:bg-red-300 mb-3 rounded-md' to="/user/adarsh">Adarsh</Link>
+        <Link className='bg-red-200 p-3 hover:bg-red-300 mb-3 rounded-md' to="/user/rohit">Rohit</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </div>
   )
 }
 
--- Routing/src/Components/UserDetail.jsx
+++ Routing/src/Components/UserDetail.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 function UserDetail() {
-   const {name} =  useParams();
-   const navigate = useNavigate();
-   const goBackHandler = ()=>{
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const goBackHandler = () => {
     navigate('/user');
-    console.log("lets go back ");   }
+  }
   return (
     <div className='w-1/2 m-auto'>
-        <h1 className='text-xl font-semibold text-[2vw] mb-10 text-red-500'>User User Details</h1>
-        <h1 className='text-xl mb-5'>Hii! {name}</h1>
-        <button onClick={goBackHandler} className='bg-red-200 rounded-md px-3 py-2'>Go Back</button>
+      <h1 className='text-xl font-semibold text-[2vw] mb-10 text-red-500'>User User Details</h1>
+      <h1 className='text-xl mb-5'>Hii! {name}</h1>
+      <button onClick={goBackHandler} className='bg-red-200 rounded-md px-3 py-2'>Go Back</button>
     </div>
   )
 }
