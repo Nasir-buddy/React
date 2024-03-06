999a6395c1d085ef51e1c41db692129fb531f3bf
nasir-buddy <syedali172000@gmail.com>
2024-03-07 01:16:16 +0530

    React context API: Fix some issue

--- Context API/src/App.jsx
+++ Context API/src/App.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react'
 import Home from './components/Home'
-import { Link, Route, Routes } from 'react-router-dom'
 import User from './components/User'
+import About from './components/About'
+import { Link, Routes, Route } from 'react-router-dom'
 
 function App() {
-  
+
 
   return (
     <div className='w-full h-screen bg-zinc-200'>
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/user'>User</Link>
-      <Link to='/about'>About</Link>
-    </nav>
+      <nav className='flex justify-center gap-3 py-2'>
+        <Link to='/'>Home</Link>
+        <Link to='/user'>User</Link>
+        <Link to='/about'>About</Link>
+      </nav>
 
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/user' element={<User/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/user' element={<User />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
 
-      <Home/>
+      <Home />
     </div>
   )
 }
