1a252441d38bc24cf8f21d71eefe0dc7dead4c9e
nasir-buddy <syedali172000@gmail.com>
2024-03-06 16:00:35 +0530

    React API: load and show data

--- Api/src/App.jsx
+++ Api/src/App.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [product, setProductData] = useState([]);
   // Data fetching function 
   const getapi = () => {
     const api = 'https://fakestoreapi.com/products';
-    axios.get(api)
+    axios
+      .get(api)
       .then((product) => {
-        console.log(product);
+        setProductData(product.data);
       })
       .catch((error) => console.log(error));
   }
@@ -27,12 +29,20 @@ function App() {
       })
       .catch((error) => console.log(error));
   }
+  console.log(product);
   return (
     <>
-    <div className='p-4 flex gap-3'>
-     <button onClick={getapi} className='px-2 py-3 bg-red-300 rounded-md'>Call API</button>
-     <button onClick={postapi} className='px-2 py-3 bg-red-300 rounded-md'>Add data to API</button>
-    </div>
+      <div className='p-4 flex gap-3'>
+        <button onClick={getapi} className='px-2 py-3 bg-red-300 rounded-md'>Call API</button>
+        <button onClick={postapi} className='px-2 py-3 bg-red-300 rounded-md'>Add data to API</button>
+      </div>
+      <hr />
+      <ul className='my-10 p-5'>
+        {product.length > 0 ?
+          product.map((p) => (
+            <li key={product.id} className='px-2 py-3 bg-red-200 mb-3 rounded-md pb-5'>{p.title}</li>
+          )) : <h1>Loading...</h1>}
+      </ul>
     </>
   )
 }
