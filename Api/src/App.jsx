7beec836ecedd798ee2653af7d85057ad5ae868c
nasir-buddy <syedali172000@gmail.com>
2024-03-06 15:12:43 +0530

    React API: send data

--- Api/src/App.jsx
+++ Api/src/App.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react'
 
 function App() {
+  // Data fetching function 
   const getapi = () => {
     const api = 'https://fakestoreapi.com/products';
     axios.get(api)
@@ -10,10 +11,27 @@ function App() {
       })
       .catch((error) => console.log(error));
   }
+  // Data posting function \
+
+  const postapi = () => {
+    const api = 'https://fakestoreapi.com/products';
+    axios.post(api, {
+      title: 'Nasir Ali',
+      price: 13.5,
+      description: 'lorem ipsum set',
+      image: 'https://i.pravatar.cc',
+      category: 'electronic'
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
+  }
   return (
     <>
-    <div className='p-4 '>
-      <button onClick={getapi} className='px-2 py-3 bg-red-300 rounded-md'>Call API</button>
+    <div className='p-4 flex gap-3'>
+     <button onClick={getapi} className='px-2 py-3 bg-red-300 rounded-md'>Call API</button>
+     <button onClick={postapi} className='px-2 py-3 bg-red-300 rounded-md'>Add data to API</button>
     </div>
     </>
   )
