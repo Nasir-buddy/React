0e72a4f010ff17ff752781542a76dae15be056fd
nasir-buddy <syedali172000@gmail.com>
2024-03-08 17:47:17 +0530

    React Project: transfer data through id from home to details components

--- Warmup Project/src/components/Home.jsx
+++ Warmup Project/src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
         <h1 className='absolute left-1/2 -transform-x-[50%] -transform-y-[50%] mt-[2.5%] font-semibold font-sans text-[1.2rem]'>Created by Nasir Ali❤️</h1>
         <div className='w-[85%] h-auto p-5 mt-[5%] flex gap-5 flex-wrap overflow-x-hidden overflow-y-auto'>
             {products.map((item, index)=>{
-                return <Link to={'/details/7'} className=' h-[21rem] w-[21rem] flex flex-col items-center justify-center rounded-lg  border shadow-xl 
+                return <Link to={`/details/${item.id}`} className=' h-[21rem] w-[21rem] flex flex-col items-center justify-center rounded-lg  border shadow-xl 
                 hover:scale-105  duration-300 p-4 bg-zinc-200'>
                 <div className='h-[80%] w-full bg-contain bg-no-repeat bg-center mb-2 rounded-lg'
                     style={{ backgroundImage: `url(${item.image})` }}></div>
