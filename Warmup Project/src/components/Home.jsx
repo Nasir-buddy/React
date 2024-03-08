4018db1875aadac1a2e26711349f20df8903b1b8
nasir-buddy <syedali172000@gmail.com>
2024-03-08 20:17:24 +0530

    React Project: fix size of card

--- Warmup Project/src/components/Home.jsx
+++ Warmup Project/src/components/Home.jsx
@@ -10,10 +10,10 @@ function Home() {
         <>
         <Nav/>
         <h1 className='absolute left-1/2 -transform-x-[50%] -transform-y-[50%] mt-[2.5%] font-semibold font-sans text-[1.2rem]'>Created by Nasir Ali❤️</h1>
-        <div className='w-[85%] h-auto p-5 mt-[5%] flex gap-5 flex-wrap overflow-x-hidden overflow-y-auto'>
+        <div className='w-[85%] h-auto p-5 mt-[5%] flex gap-5 flex-wrap overflow-x-hidden overflow-y-auto '>
             {products.map((item, index)=>{
-                return <Link to={`/details/${item.id}`} className=' h-[21rem] w-[21rem] flex flex-col items-center justify-center rounded-lg  border shadow-xl 
-                hover:scale-105  duration-300 p-4 bg-zinc-200'>
+                return <Link key={index} to={`/details/${item.id}`} className=' h-[17rem] w-[17rem] flex flex-col items-center justify-center rounded-lg  border shadow-xl 
+                hover:scale-105  duration-300 p-4 bg-zinc-200 hover:text-cyan-600'>
                 <div className='h-[80%] w-full bg-contain bg-no-repeat bg-center mb-2 rounded-lg'
                     style={{ backgroundImage: `url(${item.image})` }}></div>
                 <h1 className='font-semibold text-center'>${item.title}</h1>
