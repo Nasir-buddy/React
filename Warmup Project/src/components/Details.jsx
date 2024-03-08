b71f5ca7ffa9a8e535544de5a740d90183e3f93d
nasir-buddy <syedali172000@gmail.com>
2024-03-08 18:32:13 +0530

    React Project: add conditional rendering and show the data of the products

--- Warmup Project/src/components/Details.jsx
+++ Warmup Project/src/components/Details.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from '../context/axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Loading from './Loading';
+
 function Details() {
-  return (
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  const getSingleProduct = async () => {
+      try{
+        const { data } = await axios.get(`/products/${id}`);
+        setProduct(data);
+        console.log(data)
+      } catch(error){
+        console.log(error);
+      }
+  }
+  useEffect(()=>{
+    getSingleProduct();
+  }, []);
+
+  return product ? (
     <div className='w-[90%] h-full m-auto flex p-[10%] items-center'>
-        <img className='object-contain w-[50%] h-[100%] mr-10' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
-        <div className='ml-10 mt-[7%] text-[1.3rem] px-[5%] py-[3%] bg-zinc-200 rounded-lg h-fit'>
-            <h1 className='text-[2rem] font-semibold mb-7'> Title :  Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-            <h2 className='text-[1.5rem] font-semibold mb-7 text-red-500'>$109.95</h2>
-            <p className='text-[1.5rem] font-semibold mb-7'> Descripstion : Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
-            <h2 className='text-[1.5rem] font-semibold text-zinc-500 mb-7'> Category : men's clothing</h2>
-            <Link className='py-1 px-3 mr-5 border-[5px] border-blue-200 rounded-lg mb-3 font-semibold'>Edit</Link>
-            <Link className='py-1 px-3 border-[5px] border-red-200 rounded-lg mb-3 font-semibold'>Delete</Link>
-        </div>
-    </div>
-  )
+      <img className='object-contain w-[50%] h-[100%] mr-10' src={`${product.image}`} alt="" />
+      <div className='ml-10 mt-[7%] text-[1.3rem] px-[5%] py-[3%] bg-zinc-200 rounded-lg h-fit'>
+        <h1 className='text-[2rem] font-semibold mb-7'>{product.title}</h1>
+        <h2 className='text-[1.5rem] font-semibold mb-7 text-red-500'>$ {product.price}</h2>
+        <p className='text-[1.5rem] font-semibold mb-7'>{product.description}</p>
+        <h2 className='text-[1.5rem] font-semibold text-zinc-500 mb-7'>{product.category}</h2>
+        <Link className='py-1 px-3 mr-5 border-[5px] border-blue-200 rounded-lg mb-3 font-semibold'>Edit</Link>
+        <Link className='py-1 px-3 border-[5px] border-red-200 rounded-lg mb-3 font-semibold'>Delete</Link>
+      </div>
+    </div> 
+  ) : <Loading />
 }
 
 export default Details
