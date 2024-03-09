6b3514f4c08762e1cfe510c8c8a44895a5c0d8b6
nasir-buddy <syedali172000@gmail.com>
2024-03-10 00:19:25 +0530

    React Project: implemented personal id for products

--- Warmup Project/src/components/Create.jsx
+++ Warmup Project/src/components/Create.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
-
+import React, { useContext, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { ProductContext } from '../context/Context';
+import { nanoid } from 'nanoid';
 function Create() {
+    const [products, setProducts] = useContext(ProductContext);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [price, setPrice] = useState("");
     const [category, setCategory] = useState("");
     const [image, setImage] = useState("");
 
-    const addProductHandler = (e)=>{
+    const addProductHandler = (e) => {
         e.preventDefault();
         const product = {
-            title , image, category, price, description
-        }
-        console.log(product);
-    }
+            id: nanoid(),
+            title, image, price, category, description, 
+        };
+        setProducts([...products, product]);
+        console.log(product)
+    };
 
     return (
         <div className='h-full w-full flex p-5 justify-center'>
@@ -23,7 +28,6 @@ function Create() {
                     <input type="url"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=""
-                        required
                         onChange={(item) =>
                             setImage(item.target.value)
                         }
@@ -36,7 +40,6 @@ function Create() {
                     <input type="title"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" "
-                        required
                         onChange={(item) =>
                             setTitle(item.target.value)
                         }
@@ -51,7 +54,6 @@ function Create() {
                         <input type="category"
                             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
-                            required
                             onChange={(item) =>
                                 setCategory(item.target.value)
                             }
@@ -64,7 +66,6 @@ function Create() {
                         <input type="Price"
                             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
-                            required
                             onChange={(item) =>
                                 setPrice(item.target.value)
                             }
@@ -77,7 +78,8 @@ function Create() {
                 <div class="">
                     <div class="relative z-0 w-full mb-5 group">
                         <textarea
-                            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required
+                            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            placeholder=""
                             name="sdf"
                             cols="30" rows="10"
                             onChange={(item) =>
@@ -89,7 +91,19 @@ function Create() {
                     </div>
 
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <div className='w-1/2 flex gap-5'>
+                    <button type='submit' class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                        <span class="w-48 h-48 rounded rotate-[-40deg] bg-cyan-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                        <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Submit</span>
+                    </button>
+                    <Link to='/' class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-cyan-600 border-2 border-cyan-600 rounded-full hover:text-white group hover:bg-gray-50">
+                        <span class="absolute left-0 block w-full h-0 transition-all bg-cyan-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                        <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                        </span>
+                        <span class="relative">Go Back</span>
+                    </Link>
+                </div>
             </form>
         </div>
 
