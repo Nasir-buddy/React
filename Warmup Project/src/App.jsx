d0879dafb572829b5bf6e09d04274f816a276d3a
nasir-buddy <syedali172000@gmail.com>
2024-03-09 17:57:13 +0530

    React Project: added button to category page

--- Warmup Project/src/App.jsx
+++ Warmup Project/src/App.jsx
@@ -3,13 +3,13 @@ import Nav from './components/Nav'
 import Home from './components/Home'
 import { Link, Route, Routes } from 'react-router-dom'
 import Details from './components/Details'
+import Button from './components/Button'
 function App() {
   return (
-    
     <div className='flex w-screen h-screen '>
-      <Link to='/' className='bg-red-300 h-fit absolute p-1 rounded-md px-3'>
-        Home
-      </Link>
+
+      <Button />
+
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/details/:id' element={<Details />}></Route>
