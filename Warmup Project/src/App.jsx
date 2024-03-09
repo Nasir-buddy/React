6015347a399443921cc59019e5da525b9cdf8f36
nasir-buddy <syedali172000@gmail.com>
2024-03-09 14:14:30 +0530

    React Project: Added Home button

--- Warmup Project/src/App.jsx
+++ Warmup Project/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Home from './components/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import Details from './components/Details'
 function App() {
   return (
-
+    
     <div className='flex w-screen h-screen '>
-   
+      <Link to='/' className='bg-red-300 h-fit absolute p-1 rounded-md px-3'>
+        Home
+      </Link>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/details/:id' element={<Details />}></Route>
