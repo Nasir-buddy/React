98864cf04db4f69fbff817547926c815e86f5265
nasir-buddy <syedali172000@gmail.com>
2024-03-15 03:59:51 +0530

    React: 'initailize for redux toolkit'

---
+++ Redux Toolkit/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container mx-auto bg-red-300 mt-5 p-10 '>
+      <h1 className='text-center font-semibold text-5xl'>Let's Start with Redux Toolkit</h1>
+    </div>
+  )
+}
+
+export default App
