7ddf53c25a6e42c47db85e96aa74a051913f657e
nasir-buddy <syedali172000@gmail.com>
2024-03-16 16:29:07 +0530

    Redux: 'set counter increment decrement and increment by amount'

--- Redux Toolkit/src/App.jsx
+++ Redux Toolkit/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './store/reducers/counterSlice';
 
 function App() {
   const { value } = useSelector((state) => state.counter)
-  
+  const dispatch = useDispatch();
   return (
     <div className='container mx-auto bg-red-300 mt-5 p-10 '>
       <h1 className='text-center font-semibold mb-5 text-5xl'>Counter: {value}</h1>
       <div className='text-center'>
-          <button className='border border-gray-400 rounded-lg bg-transparent px-4 py-2 mr-5'>Increment</button>
-          <button className='border border-gray-400 rounded-lg bg-transparent px-4 py-2 mr-5'>Decrement</button>
-          <button className='border border-gray-400 rounded-lg bg-transparent px-4 py-2 '>increment by 5</button>
+          <button onClick={() => dispatch(increment())} className='border border-gray-400 rounded-lg bg-transparent px-4 py-2 mr-5'>Increment</button>
+          <button onClick={() => dispatch(decrement())} className='border border-gray-400 rounded-lg bg-transparent px-4 py-2 mr-5'>Decrement</button>
+          <button onClick={() => dispatch(incrementByAmount(5))} className='border border-gray-400 rounded-lg bg-transparent px-4 py-2 '>increment by 5</button>
       </div>
-      
     </div>
   )
 }
